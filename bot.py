3821da0fefceb60d1de131a8439f9548077954a8
Daniel Sims <drburst@gmail.com>
2020-01-14 02:35:15 -0500

    Changed code to work with new contact template

--- bot.py
+++ bot.py
@@ -243,11 +243,59 @@ async def on_message(message):
 			contact_settings["-type"] == "Service" or \
 			contact_settings["-type"] == "Legwork" or \
 			contact_settings["-type"] == "Networking" or \
-			contact_settings["-type"] == "Generalist":
+			contact_settings["-type"] == "Generalist" or \
+			contact_settings["-type"] == "Custom(K,A,G,N)" or \
+			contact_settings["-type"] == "Custom(K,A,N,G)" or \
+			contact_settings["-type"] == "Custom(K,G,A,N)" or \
+			contact_settings["-type"] == "Custom(K,G,N,A)" or \
+			contact_settings["-type"] == "Custom(K,N,A,G)" or \
+			contact_settings["-type"] == "Custom(K,N,G,A)" or \
+			contact_settings["-type"] == "Custom(A,K,G,N)" or \
+			contact_settings["-type"] == "Custom(A,K,N,G)" or \
+			contact_settings["-type"] == "Custom(A,G,K,N)" or \
+			contact_settings["-type"] == "Custom(A,G,N,K)" or \
+			contact_settings["-type"] == "Custom(A,N,K,G)" or \
+			contact_settings["-type"] == "Custom(A,N,G,K)" or \
+			contact_settings["-type"] == "Custom(G,K,A,N)" or \
+			contact_settings["-type"] == "Custom(G,K,N,A)" or \
+			contact_settings["-type"] == "Custom(G,A,K,N)" or \
+			contact_settings["-type"] == "Custom(G,A,N,K)" or \
+			contact_settings["-type"] == "Custom(G,N,K,A)" or \
+			contact_settings["-type"] == "Custom(G,N,A,K)" or \
+			contact_settings["-type"] == "Custom(N,K,A,G)" or \
+			contact_settings["-type"] == "Custom(N,K,G,A)" or \
+			contact_settings["-type"] == "Custom(N,A,K,G)" or \
+			contact_settings["-type"] == "Custom(N,A,G,K)" or \
+			contact_settings["-type"] == "Custom(N,G,K,A)" or \
+			contact_settings["-type"] == "Custom(N,G,A,K)" or \
+			contact_settings["-type"] == "Fixer(K,A,G,N)" or \
+			contact_settings["-type"] == "Fixer(K,A,N,G)" or \
+			contact_settings["-type"] == "Fixer(K,G,A,N)" or \
+			contact_settings["-type"] == "Fixer(K,G,N,A)" or \
+			contact_settings["-type"] == "Fixer(K,N,A,G)" or \
+			contact_settings["-type"] == "Fixer(K,N,G,A)" or \
+			contact_settings["-type"] == "Fixer(A,K,G,N)" or \
+			contact_settings["-type"] == "Fixer(A,K,N,G)" or \
+			contact_settings["-type"] == "Fixer(A,G,K,N)" or \
+			contact_settings["-type"] == "Fixer(A,G,N,K)" or \
+			contact_settings["-type"] == "Fixer(A,N,K,G)" or \
+			contact_settings["-type"] == "Fixer(A,N,G,K)" or \
+			contact_settings["-type"] == "Fixer(G,K,A,N)" or \
+			contact_settings["-type"] == "Fixer(G,K,N,A)" or \
+			contact_settings["-type"] == "Fixer(G,A,K,N)" or \
+			contact_settings["-type"] == "Fixer(G,A,N,K)" or \
+			contact_settings["-type"] == "Fixer(G,N,K,A)" or \
+			contact_settings["-type"] == "Fixer(G,N,A,K)" or \
+			contact_settings["-type"] == "Fixer(N,K,A,G)" or \
+			contact_settings["-type"] == "Fixer(N,K,G,A)" or \
+			contact_settings["-type"] == "Fixer(N,A,K,G)" or \
+			contact_settings["-type"] == "Fixer(N,A,G,K)" or \
+			contact_settings["-type"] == "Fixer(N,G,K,A)" or \
+			contact_settings["-type"] == "Fixer(N,G,A,K)":
 				pass
 			else:
 				contact_valid = False
-				await message.channel.send("Invalid contact type {}, must be one of the following: Fixer, Gear, Service, Legwork, Networking, Generalist".format(contact_settings["-type"]))
+				await message.channel.send("Invalid contact type {}, must be one of the following: Fixer, Gear, Service, Legwork, Networking, Generalist or a valid custom or custom fixer contact".format(contact_settings["-type"]))
 		if contact_valid:
 			raw_aspects = contact_settings["-aspects"]
 			aspects = shlex.split(raw_aspects)
@@ -309,8 +357,10 @@ async def on_message(message):
 				aspects_string = aspects_string + "*[[hasAspect::{}]]<BR>\n".format(aspect)
 			aspects_string = aspects_string[:-5]
 			for aspect in aspects:
-				aspects_description_string = aspects_description_string + "\'\'\'{}\'\'\' - Please describe how this aspect relates to this contact\n\n".format(aspect)
-			aspects_description_string = aspects_description_string[:-1]
+				aspects_description_string =aspects_description_string + "{{PositiveAspect|\n" +
+						"|Aspect={}\n" +
+						"|Description=This is an example positive aspect. You can write the fluff in here. Duplicate this section as many times as needed for your contact.\n" +
+						"|}}\n" +.format(aspect)
 
 			# Step 4: POST request to edit a page
 			PARAMS_3 = {
@@ -318,7 +368,79 @@ async def on_message(message):
 				"title": contact_settings["-name"],
 				"token": CSRF_TOKEN,
 				"format": "json",
-				"appendtext": "{{{{Infobox\n|title = [[has name::{name_string}]]\n|image= [[File:Default-welcomer.png|200px]]\n|header1= [[Profession::Undefined]]\n|header2=\n|header3=\n|header4=\n|label5 = Contact Owner\n|data5 = \n|label6 = Connection\n|data6 = [[Connection::{connection_string}]]\n|label9 = Aspects\n|data9 = {aspects_string}\n|label10 = Negative Aspects\n|data10 = {negative_aspects_string}\n|label30 = Public Contact?\n|data30= Yes\n|label7 = Archetype\n|data7 =  [[Archetype::{type_string}]]\n|label8 = Faction\n|data8 = [[Faction::{faction_string}]]\n|label11 = Location\n|data11= [[Location::Undefined]]\n|label12 = Metatype\n|data12 = [[Metatype::Undefined]]\n|label13 = Gender\n|data13 = [[Gender::Undefined]]\n|label14 = Age\n|data14= Undefined\n|label15= Preferred Payment Method\n|data15 = Undefined\n|label20 =  Hobbies/Vice\n|data20 = Undefined\n|label17 = Personal Life\n|data17 = Undefined\n}}}}\n==Overview==\n\nFill this out\n\n==Aspects Description==\n\n{aspect_description}\n\n==Network==\n\n===Characters with this Contact ===\n\n{{ContactPlayerConnections}}\n|mainlabel=-\n|format=table\n|headers=plain\n|default=None\n|link=subject\n|?=Name\n|?Loyalty{{{{PAGENAME}}}}=Loyalty\n|?Chips{{{{PAGENAME}}}}=Chips\n|limit=300\n}}}}\n[[Category:Contacts]]\n[[Category:Public Contacts]]\n__SHOWFACTBOX__".format(name_string=contact_settings["-name"],connection_string=contact_settings["-connection"],type_string=contact_settings["-type"],faction_string=contact_settings["-faction"],aspects_string=aspects_string,negative_aspects_string=negative_aspects_string,aspect_description=aspects_description_string)
+				"appendtext": "{{Infobox\n" +
+							"|title = {{PAGENAME}}  <!-- You don't need to change the title. This auto-generates for you. -->\n" + 
+							"|image= [[File:Placeholder2.jpg|200px]] <!-- You can upload your own image for your contact. Replace Placeholder2.jpg with the name of the image you'd like to use. After you create the page, click on the red link to upload the image. -->\n" +
+							"|header1= [[Profession::Undefined]] <!-- This is the \"official job\" of the contact. It is a good way to describe their skills and what sets them apart from all of the other contacts of the same Archetype. -->\n" +
+							"|header2= (Shortblurb) <!-- You can write a short description here. Also on the next couple header lines if you want. -->\n" +
+							"|header3= \n" +
+							"|header4= \n" +
+							"|label5 = Contact Owner <!-- Leave this alone --> \n" +
+							"|data5 = [https://www.reddit.com/user/Your_Handle_Here Your_Handle_Here] <!-- Here you can put your reddit name. Be sure to replace BOTH instances of Your_Handle_Here with your reddit name. -->\n" +
+							"|label6 = Connection <!-- Leave this alone -->\n" +
+							"|data6 = [[Connection::{connection_string}]] <!-- This is the connection for your contact. Remember that at chargen, your connection+loyalty can not exceed 7, raising contacts above connection 6 post-gen requires thematics approval. -->\n" +
+							"|label8 = Public Contact? <!-- Leave this alone -->\n" +
+							"|data8= Yes <!-- If you want the contact to be public, where anyone can gain the contact, leave this alone. If you want the contact to be private where only your character has the contact, change this to \"no\" and remove Category:Public Contacts from the bottom of the page. -->\n" +
+							"|label9 = [[Contact_Rules#Contact_Archetype|Archetype]] <!-- Leave this alone -->\n" +
+							"|data9 =  [[Archetype::{type_string}]] <!-- This is where you define the Archetype. Default Archetypes are:  Fixer, Service, Gear, Legwork, Networking, Generalist. Custom Archetypes may be defined as denoted on the contact rules page. -->\n" +
+							"|label10 = Location <!-- Leave this alone -->\n" +
+							"|data10= [[Location::XXX, XXX]] <!-- Where is your contact normally found? -->\n" +
+							"|label11 = Metatype <!-- Leave this alone -->\n" +
+							"|data11 = [[Metatype::Undefined]] <!-- Human, Elf, Ork, Naga, AI who inhabits an Animatronic Dancing Penguin Drone, ect. -->\n" +
+							"|label12 = Sex <!-- Leave this alone -->\n" +
+							"|data12 = [[Gender::Undefined]] <!-- You can set the gender here. But please don't be a wiseass and put "yes please" under Sex. We've done that joke already. -->\n" +
+							"|label13 = Age <!-- Leave this alone -->\n" +
+							"|data13= CONTACT AGE HERE <!-- How old is your contact? You can give them a specific age like 44, or a general age like \"mid-40s\" or \"middle-aged\" -->\n" +
+							"|label14= Preferred Payment Method <!-- Leave this alone -->\n" +
+							"|data14 = (Cash) <!-- How do they like to be compensated? -->\n" +
+							"|label15 =  Hobbies/Vice <!-- Leave this alone -->\n" +
+							"|data15 = Blackjack, and Hookers. Actually, forget the blackjack. <!-- What interests them? -->\n" +
+							"|label16 = Personal Life <!-- Leave this alone --> \n" +
+							"|data16 = Single/Divorced/None of your business <!-- This kinda explains itself. --> \n" +
+							"|label17 = Faction <!-- Leave this alone -->\n" +
+							"|data17 = [[Faction::{faction_string}]] <!-- If they belong to a faction or group, fill it in here by replacing XXX. Otherwise, delete this entire line. -->\n" +
+							"|label19 = Aspects <!-- Leave this alone -->\n" +
+							"|data19 = {{GenerateAspectList}}  <!-- Leave this alone -->\n\n" +
+							"<!-- The above aspects area is important. Replace the XXX with the aspects of your choosing. You can add more if you need to. -->\n" +
+							"}}\n\n\n" +
+
+
+							"==Overview==\n" +
+							"This is where you would write a blurb about the contact.\n\n" +
+
+							"==Aspects Description==\n" +
+							"<!-- Do not remove the below text. It's super important for the Aspect table. -->\n" +
+							"{{AspectTable|\n" +
+							"<!-- Do not remove the above code. It's super important. You can add new aspects below using the PositiveAspect and NegativeAspect template you see below. To add additional aspects, simply copy/paste one of the blocks below and update it with the name of the aaspect, and a short description.  -->\n\n\n" +
+
+
+						"<!-- Below is an example of a positive aspect. You may duplicate this as many times as necessary. -->\n" +
+						"{{PositiveAspect|\n" +
+						"|Aspect=Example Positive Aspect\n" +
+						"|Description=This is an example positive aspect. You can write the fluff in here. Duplicate this section as many times as needed for your contact.\n" +
+						"|}}\n" +
+						"<!-- Above is an example of a positive aspect. You may duplicate this as many times as necessary. -->\n\n\n" +
+
+
+
+						"<!-- Do not remove the below text. It's super important for the Aspect table. All aspects should be listed above this line. -->\n" +
+						"}}\n" +
+						"<!-- Do not remove the below text. -->\n" +
+
+						"==Dicepools==\n" +
+						"{{ContactDicepools}}\n" +
+						"<!-- You do not need to mess with this. This template should automatically generate a table with dicepools based on the Archetype and the connection. If it comes up as a broken red link, don't panic and ask @wiki for help on the discord.-->\n\n" +
+
+						"==Network==\n" +
+						"===Player Characters with this Contact===\n" +
+						"{{ContactPlayerConnections}}\n" +
+						"<!-- You do not need to mess with this. This template should automatically generate a table with everyone who has the contact on their wiki page. -->\n\n" +
+
+						"===NPC who know this contact===\n" +
+						"[[Category:Contacts]]\n" +
+						"[[Category:Public Contacts]]\n\n" +
+
+						"__SHOWFACTBOX__".format(name_string=contact_settings["-name"],connection_string=contact_settings["-connection"],type_string=contact_settings["-type"],faction_string=contact_settings["-faction"],aspects_string=aspects_string,negative_aspects_string=negative_aspects_string)
 			}
 
 
@@ -329,6 +451,47 @@ async def on_message(message):
 
 			print(DATA)
 			await message.channel.send('Contact Made, see https://www.shadowhaven.info/{name_string}'.format(name_string=contact_settings["-name"]))
+	if message.content.startswith('$make_wiki_account'):
+		S = requests.Session()
 
+		URL = "https://www.shadowhaven.info/api.php"
+
+		# Step 1: GET request to fetch login token
+		PARAMS_0 = {
+			"action": "query",
+			"meta": "tokens",
+			"type": "login",
+			"format": "json"
+		}
+
+		R = S.get(url=URL, params=PARAMS_0)
+		DATA = R.json()
+
+		LOGIN_TOKEN = DATA['query']['tokens']['logintoken']
+
+		# Step 2: POST request to log in. Use of main account for login is not
+		# supported. Obtain credentials via Special:BotPasswords
+		# (https://www.mediawiki.org/wiki/Special:BotPasswords) for lgname & lgpassword
+		PARAMS_1 = {
+			"action": "login",
+			"lgname": script_config['Wiki_Username'],
+			"lgpassword": script_config['Wiki_Password'],
+			"lgtoken": LOGIN_TOKEN,
+			"format": "json"
+		}
+
+		R = S.post(URL, data=PARAMS_1)
+
+		# Step 3: GET request to fetch CSRF token
+		PARAMS_2 = {
+			"action": "query",
+			"meta": "tokens",
+			"format": "json",
+			'type': 'createaccount',
+		}
+		R = S.get(url=URL, params=PARAMS_2)
+		DATA = R.json()
+
+		CSRF_TOKEN = DATA['query']['tokens']['csrftoken']
 
 client.run(script_config['Discord_API_Key'])
